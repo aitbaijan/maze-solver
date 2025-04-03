faba67cf90ec6d9437021171fc9ab1572b877894
Aizhan <ayjan23d06@gmail.com>
2025-04-03 20:39:00 +0600

    Генерация лабиринта. Стартовая и выходная точка

--- maze-solver/import random.py
+++
@@ -1,4 +0,0 @@
-import random
-def create_empty_maze(rows, cols):
-    return [['#' for _ in range(cols)] for _ in range(rows)]
-    
---
+++ maze-solver/maze-solver.py
@@ -0,0 +1,5 @@
+import random
+
+def generate_maze(rows, cols):
+    start = (0, 0)
+    end = (rows - 1, cols - 1)
